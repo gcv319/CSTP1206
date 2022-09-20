8c0c596741cf2cdc00a179592739fc06d5b5fd9f
Gian Viterbo <gioviterbo@gmail.com>
2022-09-20 10:42:24 -0700

    made session3

---
+++ CSTP1206_Session3/01-variable.js
@@ -0,0 +1,37 @@
+function sum () {
+    var a;
+    var b; // Declaration
+    a = 10;
+
+    console.log(b, "Value of b");
+
+    if (true) {
+        b = 11; // Assignment/Call
+        let c = 10; // died within the block
+        const d = 20; // died within the block
+    }
+    
+    // console.log(a); // 10
+    // console.log(b); // 11
+    // console.log(c); // Error --> Referencial Error
+    // console.log(d); // Error --> Referencial Error
+    // c & d are in the temporal dead zone
+}
+
+sum();
+
+function random() {
+    let a = 5;
+    const d = 5;
+
+    if (true) {
+        let a = 10;
+        if (true) {
+            console.log(a, "inside");
+        }
+    }
+
+    console.log(a, "outside");
+}
+
+random();
