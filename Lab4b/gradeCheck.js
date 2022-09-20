c3a108206ab5574fd9f38a2336fabe46ea91188f
Gian Viterbo <gioviterbo@gmail.com>
2022-09-19 22:33:33 -0700

    Add lab4b

---
+++ Lab4b/gradeCheck.js
@@ -0,0 +1,34 @@
+grade = 96;
+
+if (grade >= 95){
+    console.log("A")
+}
+else if (grade >= 90 && grade < 95){
+    console.log ("B+")
+}
+else if (grade >= 85 && grade < 90){
+    console.log ("B")
+}
+else if (grade >= 80 && grade < 85){
+    console.log ("B+")
+}
+else if (grade >= 75 && grade < 80){
+    console.log ("B")
+}
+else if (grade >= 70 && grade < 75){
+    console.log ("B-")
+}
+else if (grade >= 65 && grade < 70){
+    console.log ("C+")
+}
+else if (grade >= 60 && grade < 65){
+    console.log ("C")
+}
+else if (grade >= 50 && grade < 60){
+    console.log ("C-")
+}
+else {
+    console.log ("F")
+}
+
+console.log("Goodbye!")
