d4a5dc2f7ba0ef5ade2c91f70ed03ce65da724dc
Gian Viterbo <gioviterbo@gmail.com>
2022-10-01 13:47:58 -0700

    Completed the HTML and JS portion of assignment

--- 1206_classWork/assignment01/script.js
+++ 1206_classWork/assignment01/script.js
@@ -2,9 +2,26 @@
 const enterButton = document.querySelector("#enterButton");
 console.log(enterButton);
 
-const userInfo = document.querySelector("#userInfo");
+// This function will insert a new row with the given info into the table
+const insertUserInfo = (name, email, id) => {
+    // I put the paramters into an object so we can access it later
+    let tempStorage = {name, email, id};
 
+    let tableInfo = document.getElementById("userInfo");
+    let newRow = tableInfo.insertRow();
+
+    // This goes through the object which contains the data
+    // and it adds it to the HTML table
+    for (let value in tempStorage) {
+        let newData = newRow.insertCell();
+        newData.innerHTML = tempStorage[value]; 
+    }
+}
 // When the button is clicked we will perform gather the user information and save it to the table
 enterButton.addEventListener("click", () => {
-    console.log("Got info");
+    let nameValue = document.getElementById("studentName").value;
+    let emailValue = document.getElementById("studentEmail").value;
+    let studentIDValue = document.getElementById("studentID").value;
+
+    insertUserInfo(nameValue, emailValue, studentIDValue);
 })
