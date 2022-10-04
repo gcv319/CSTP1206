fa3364762ff979288adb64685e2edd99ed3303b6
Gian Viterbo <gioviterbo@gmail.com>
2022-10-04 10:45:07 -0700

    ignore node_modules

--- 1206_classWork/sessions/CSTP1206_Session5/index.js
+++ 1206_classWork/sessions/CSTP1206_Session5/index.js
@@ -4,15 +4,27 @@ const PORT = 4000;
 
 // Create a server
 const server = http.createServer((request, response) => {
-    
+    response.setHeader("Content-type", "text/html");
+
     if (request.url === "/") {
         // Setting headers to text and html so the browser what
         // kind of response I will recieve
-        response.setHeader("Content-type", "text/html");
         
         // We are sending a response to the client/browser using
         // response.write();
-        response.write("<h1>Hello thank you for contacting me! </h1>");
+        response.write("<h1>Hello thank you for contacting me!</h1>");
+
+        // Ending the response
+        response.end();
+    }
+
+    if (request.url === "/user") {
+        // Setting headers to text and html so the browser what
+        // kind of response I will recieve
+        
+        // We are sending a response to the client/browser using
+        // response.write();
+        response.write("<h1>Hello user!</h1>");
 
         // Ending the response
         response.end();
