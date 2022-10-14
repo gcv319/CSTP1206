b0751297d296707d2779489752106d6fc39ed3ea
Gian Viterbo <gioviterbo@gmail.com>
2022-10-14 14:44:55 -0700

    working on question02.js

--- 1206_classWork/assignmentWeek06/programQuestions/question02.js
+++ 1206_classWork/assignmentWeek06/programQuestions/question02.js
@@ -1,2 +1,9 @@
 let array1 = [4, 5, 2, 1, 0];
 let array2 = [2, 1, 0, 3, 7, 6, 4, 5, 10, 9];
+
+function missingNumber(array) {
+  let maxNum = Math.max(...array);
+}
+
+missingNumber(array1);
+missingNumber(array2);
