f8ace3c6cedabf8286ee23f085a18a065ee47899
Gian Viterbo <gioviterbo@gmail.com>
2022-10-15 14:58:01 -0700

    completed question02 but I need to change it so that I am working with HOF, but I understand the concept

--- 1206_classWork/assignmentWeek06/programQuestions/question02.js
+++ 1206_classWork/assignmentWeek06/programQuestions/question02.js
@@ -2,8 +2,18 @@ let array1 = [4, 5, 2, 1, 0];
 let array2 = [2, 1, 0, 3, 7, 6, 4, 5, 10, 9];
 
 function missingNumber(array) {
+  let missing = [];
   let maxNum = Math.max(...array);
+  let minNum = Math.min(...array);
+
+  for (let i = minNum; i <= maxNum; i++) {
+    if (!array.includes(i)) {
+      missing.push(i);
+    }
+  }
+
+  return missing;
 }
 
-missingNumber(array1);
-missingNumber(array2);
+console.log(missingNumber(array1));
+console.log(missingNumber(array2));
