9821ffe367de4c61bc9c45636183d356387927d8
Gian Viterbo <gioviterbo@gmail.com>
2022-10-15 16:17:10 -0700

    created an express sever with the Employee object. I now need to make it so that we can add multiple objects inside of Employee inside the server

--- 1206_classWork/assignmentWeek06/Express/index.js
+++ 1206_classWork/assignmentWeek06/Express/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const PORT = 3000;
 
+let Employee = [
+  {
+    "Name": "Daniel",
+    "empoyeeId": 100,
+    "Email": "daniel@gmail.com",
+    "Department": "IT"
+  }
+];
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/employees', (req, res) => {
+  res.status(200).json(Employee);
+});
+
+app.post('/employees', (req, res) => {
+  Employee.push(req.body);
+  res.status(201).json(Employee);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
