e811e622589340dc9d72d1f7f55524c494a118cc
gcv319 <105512041+gcv319@users.noreply.github.com>
2022-10-16 21:38:48 -0700

    revised question02 but still unsure how to implement HOF in both q1 and q2 will need to ask in class

--- 1206_classWork/assignmentWeek06/programQuestions/question02.js
+++ 1206_classWork/assignmentWeek06/programQuestions/question02.js
@@ -1,12 +1,16 @@
-let array1 = [4, 5, 2, 1, 0];
-let array2 = [2, 1, 0, 3, 7, 6, 4, 5, 10, 9];
+let array1 = [4, 5, 2, 1, 0]; // 3
+let array2 = [2, 1, 0, 3, 7, 6, 4, 5, 10, 9]; // 8
 
+// finds the missing number in the array
 function missingNumber(array) {
+  // an empty array which we will use to put all the missing numbers in
   let missing = [];
+  // we get the max num of the array
   let maxNum = Math.max(...array);
-  let minNum = Math.min(...array);
 
-  for (let i = minNum; i <= maxNum; i++) {
+  // this for loop goes from 0 to the maxNum of the array and checks if
+  // it is in the array or not; if not we push it to the missing array
+  for (let i = 0; i <= maxNum; i++) {
     if (!array.includes(i)) {
       missing.push(i);
     }
