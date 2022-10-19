19ec5a98bbbf4c6c969f5aaf68407b037d47843d
Gian Viterbo <gioviterbo@gmail.com>
2022-10-18 18:56:06 -0700

    completed question02.js without using any for loops and instead used HOF

--- 1206_classWork/assignmentWeek06/programQuestions/question02.js
+++ 1206_classWork/assignmentWeek06/programQuestions/question02.js
@@ -4,17 +4,35 @@ let array2 = [2, 1, 0, 3, 7, 6, 4, 5, 10, 9]; // 8
 // finds the missing number in the array
 function missingNumber(array, n) {
   // an empty array which we will use to put all the missing numbers in
-  let missing = [];
+  // let missing = [];
 
   // this for loop goes from 0 to the maxNum of the array and checks if
   // it is in the array or not; if not we push it to the missing array
-  for (let i = 0; i <= n; i++) {
-    if (!array.includes(i)) {
-      missing.push(i);
-    }
-  }
+  // for (let i = 0; i <= n; i++) {
+  //   if (array.indexOf(i) == -1) {
+  //     missing.push(i);
+  //   }
+  // }
 
-  return missing;
+  // return missing;
+
+  // array.sort((a, b) => { return a - b });
+
+  // const missingNum = array.findIndex((num, index) => {
+  //   if (num !== index) {
+  //     return index;
+  //   }
+  // })
+
+  // return missingNum;
+
+  // This gets the sum of all the numbers from 0 -> n
+  let sum = (n + 1) * (n) / 2;
+
+  // This reduces the array we the actual sum and subtracts it with the maximum
+  // theoretical sum of 0 -> n; therefore getting our missing number
+  let missingNum = sum - array.reduce((x, y) => x + y);
+  return missingNum;
 }
 
 console.log(missingNumber(array1, 5));
