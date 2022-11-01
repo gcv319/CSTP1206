bfd6ba566c3d519b8a1620429f98e71acd474169
Gian Viterbo <gioviterbo@gmail.com>
2022-11-01 15:33:47 -0700

    completed the ejs portion of assignment 7 now need to work on the css

--- 1206_classWork/assignmentWeek07/app.js
+++ 1206_classWork/assignmentWeek07/app.js
@@ -2,16 +2,32 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 
+let today = new Date();
+
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = today.getMonth();
+let yyyy = today.getFullYear();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+today = `${months[mm]} ${dd}, ${yyyy}`;
+
 let userInfo = {
   name: "Gio",
   email: "gio@vcc.ca",
-  date: new Date()
+  date: today,
+  phoneNumber: 6041237890,
+  social: {
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/769px-Instagram-Icon.png": "https://www.instagram.com/",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4n_urpJ9XpwOTdzBVbGvactwHrPagYQrTJPYjxfxLGkSyu7nJZVqRVGAeohnPgKMrnKE&usqp=CAU": "https://twitter.com/",
+    "https://upload.wikimedia.org/wikipedia/commons/4/44/Facebook_Logo.png": "https://www.facebook.com/"
+  }
 }
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { name: "Gio" })
+  res.render('index', { data: userInfo });
 })
 
 app.listen(PORT, (req, res) => {
