c1434aae2baab62e8f42e5de1cd1f46a21998756
Gian Viterbo <gioviterbo@gmail.com>
2022-11-01 12:21:07 -0700

    completed mongodb intro

--- 1206_classWork/sessions/CSTP1206_Session10/Intro_to_Mongodb/server.js
+++ 1206_classWork/sessions/CSTP1206_Session10/Intro_to_Mongodb/server.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// User Model 
+const User = require('./models/user');
+
+// So we can accept json information
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI, (error) => {
   if (error) {
     console.error(`There was an error: ${error}`);
@@ -16,6 +22,39 @@ mongoose.connect(process.env.MONGO_URI, (error) => {
 app.get('/', (req, res) => {
   res.send('USER ENDPOINTS!');
 });
+
+app.get('/users', (req, res) => {
+  // returns async response; // This returns a promise
+  // then is waiting for the response
+  // User helps me get hold of information from mongodb atlas server
+  User.find().then((data) => {
+    return res.status(200).json({
+      message: "Successfully fetched",
+      data
+    });
+  });
+});
+
+app.post('/users', (req, res) => {
+  let newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber
+  }
+
+  // We are making an instance of User Model
+  const user = new User(newUser);
+
+  // Save method helps you save data in database
+  user.save().then((data) => {
+    return res.status(201).json({
+      message: "User successfully created",
+      data
+    })
+  })
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`)
