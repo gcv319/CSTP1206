c41c7ca20f2939803f20d7666b77d33822289672
Gian Viterbo <gioviterbo@gmail.com>
2022-11-09 20:29:52 -0800

    got css to work on my ejs file

--- 1206_classWork/assignmentWeek07/app.js
+++ 1206_classWork/assignmentWeek07/app.js
@@ -13,10 +13,10 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 today = `${months[mm]} ${dd}, ${yyyy}`;
 
 let userInfo = {
-  name: "Gio",
-  email: "gio@vcc.ca",
-  date: today,
-  phoneNumber: 6041237890,
+  name: "name: Gio",
+  email: "email: gio@vcc.ca",
+  date: "date: " + today,
+  phoneNumber: "number: " + 6041237890,
   social: {
     "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/769px-Instagram-Icon.png": "https://www.instagram.com/",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4n_urpJ9XpwOTdzBVbGvactwHrPagYQrTJPYjxfxLGkSyu7nJZVqRVGAeohnPgKMrnKE&usqp=CAU": "https://twitter.com/",
@@ -25,6 +25,9 @@ let userInfo = {
 }
 
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
 
 app.get('/', (req, res) => {
   res.render('index', { data: userInfo });
