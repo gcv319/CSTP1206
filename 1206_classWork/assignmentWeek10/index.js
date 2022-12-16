0a2c6734114ec284379f66af1b53f24cadc86c0a
Gian Viterbo <gioviterbo@gmail.com>
2022-12-16 10:32:01 -0800

    successfully gotten the github api and display it to the html page now I need to do some css and add a link when someone clicks the pfp

--- 1206_classWork/assignmentWeek10/index.js
+++ 1206_classWork/assignmentWeek10/index.js
@@ -1,3 +1,27 @@
-fetch('https://api.github.com/repos/facebook/react/issues')
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[user-card-container]");
+const searchInput = document.querySelector("[data-search]");
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  console.log(value);
+})
+
+fetch("https://api.github.com/repos/facebook/react/issues")
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    data.forEach(element => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const pfp = card.querySelector("[data-userPfp]");
+      const issue = card.querySelector("[data-issue]");
+      const tags = card.querySelector("[data-tags]");
+      pfp.src = element.user.avatar_url;
+      issue.textContent = element.title;
+      element.labels.forEach(label => {
+        console.log(label.name);
+        tags.textContent += label.name + "\n";
+      });
+      userCardContainer.append(card);
+    });
+    console.log(data);
+  });
