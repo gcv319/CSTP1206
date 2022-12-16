b6d374a25114666439d2a6eb1ac2eee3f3945b8a
Gian Viterbo <gioviterbo@gmail.com>
2022-12-16 11:19:44 -0800

    was able to get the tags search to work but they are case sensitive and only works if you input the exact value

--- 1206_classWork/assignmentWeek10/index.js
+++ 1206_classWork/assignmentWeek10/index.js
@@ -7,9 +7,8 @@ let users = [];
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value;
   users.forEach(user => {
-    const isVisible = user.issue.toUpperCase().includes(value.toUpperCase());
+    const isVisible = user.issue.toUpperCase().includes(value.toUpperCase()) || user.tags.includes(value);
     user.element.classList.toggle("hide", !isVisible);
-    console.log(users);
   })
 })
 
@@ -17,6 +16,7 @@ fetch("https://api.github.com/repos/facebook/react/issues")
   .then(res => res.json())
   .then(data => {
     users = data.map(element => {
+      let objLabels = [];
       const card = userCardTemplate.content.cloneNode(true).children[0];
       const pfp = card.querySelector("[data-userPfp]");
       const issue = card.querySelector("[data-issue]");
@@ -25,8 +25,9 @@ fetch("https://api.github.com/repos/facebook/react/issues")
       issue.textContent = element.title;
       element.labels.forEach(label => {
         tags.textContent += label.name + "\n";
+        objLabels.push(label.name);
       });
       userCardContainer.append(card);
-      return { issue: element.title, tags: element.labels.name, element: card }
+      return { issue: element.title, tags: objLabels, element: card }
     });
   });
